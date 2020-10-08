2a49ffd7625637545041e9ff0f39ec7e38391d6e
Braulio Ribeiro <brarrunategui@gmail.com>
2020-10-08 18:10:36 +0200

    tests

--- tests/test_zeppelin.py
+++ tests/test_zeppelin.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 
 """Tests for `zeppelin` package."""
-
 import pytest
 import os.path
-from zeppelin.scrape_lyrics import save_all_lyrics
-from zeppelin.create_lyricscorpus import create_corpuslist
-from zeppelin.model import vectors_and_df
-from zeppelin.model import predict
+from ZEPPELIN.zeppelin import scrape_lyrics
+from ZEPPELIN.zeppelin import create_lyricscorpus
+from ZEPPELIN.zeppelin import model
 
 # decorators to get the links
 @pytest.fixture
 def link():
     url = 'https://www.lyrics.com/artist/Koffee/3491656'
-    url, directory = save_all_lyrics(url, 'Koffee')
+    url, directory = scrape_lyrics.save_all_lyrics(url, 'Koffee')
     return directory
 
 
 @pytest.fixture
 def link_2():
     url = 'https://www.lyrics.com/artist/MF-Doom/300089'
-    url, directory = save_all_lyrics(url, 'MFDOOM')
+    url, directory = scrape_lyrics.save_all_lyrics(url, 'MFDOOM')
     return directory
 
 # test generating correct numbers of sub-directories
@@ -32,7 +30,7 @@ PASSING_CONDITIONS2 = ['Koffee', 'MF DOOM', 'Danger Doom']
 
 @pytest.mark.parametrize("url", PASSING_CONDITIONS, "name", PASSING_CONDITIONS2)
 def test_save_all_lyrics(url, name):
-    url, name = save_all_lyrics(url, name)
+    url, name = scrape_lyrics.save_all_lyrics(url, name)
     files_list = os.listdir('lyrics-files/')
     assert len(files_list) == 3
 
@@ -42,6 +40,6 @@ def test_create_corpuslist(name):
 
 # test the possible outcomes of the predictions and the merge and train function
 def test_model():
-    df, vectorizer = vectors_and_df(CORPUS, LABEL)
-    prediction = predict("ANY LYRICS")
+    df, vectorizer = model.vectors_and_df(CORPUS, LABEL)
+    prediction = model.predict("ANY LYRICS")
     assert prediction == ['Koffee'] or prediction == ['MF DOOM']
