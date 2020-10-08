a885b6740eb015818611b035509f61ab9125a7c2
Braulio Ribeiro <brarrunategui@gmail.com>
2020-10-08 18:53:18 +0200

    tests update

--- tests/test_zeppelin.py
+++ tests/test_zeppelin.py
@@ -3,22 +3,22 @@
 """Tests for `zeppelin` package."""
 import pytest
 import os.path
-from ZEPPELIN.zeppelin import scrape_lyrics
-from ZEPPELIN.zeppelin import create_lyricscorpus
-from ZEPPELIN.zeppelin import model
+from zeppelin.scrape_lyrics import save_all_lyrics
+from zeppelin.create_lyricscorpus import create_corpuslist
+from zeppelin.model import vectors_and_df, predict
 
 # decorators to get the links
 @pytest.fixture
 def link():
     url = 'https://www.lyrics.com/artist/Koffee/3491656'
-    url, directory = scrape_lyrics.save_all_lyrics(url, 'Koffee')
+    url, directory = save_all_lyrics(url, 'Koffee')
     return directory
 
 
 @pytest.fixture
 def link_2():
     url = 'https://www.lyrics.com/artist/MF-Doom/300089'
-    url, directory = scrape_lyrics.save_all_lyrics(url, 'MFDOOM')
+    url, directory = save_all_lyrics(url, 'MFDOOM')
     return directory
 
 # test generating correct numbers of sub-directories
@@ -30,17 +30,17 @@ NAME = ['Koffee', 'MF DOOM', 'Danger Doom']
 
 @pytest.mark.parametrize("url", PASSING_CONDITIONS, "name", PASSING_CONDITIONS2)
 def test_save_all_lyrics(url, name):
-    url, name = scrape_lyrics.save_all_lyrics(url, name)
+    url, name = save_all_lyrics(url, name)
     files_list = os.listdir('lyrics-files/')
     assert len(files_list) == 3
 
 # test if the corpus lists have correct size
 def test_create_corpuslist(name):
-    create_lyricscorpus.create_corpuslist(NAME)
+    create_corpuslist(NAME)
     assert len(CORPUS) == len(LABEL)
 
 # test the possible outcomes of the predictions and the merge and train function
 def test_model():
-    df, vectorizer = model.vectors_and_df(CORPUS, LABEL)
-    prediction = model.predict("ANY LYRICS")
+    df, vectorizer = vectors_and_df(CORPUS, LABEL)
+    prediction = predict("ANY LYRICS")
     assert prediction == ['Koffee'] or prediction == ['MF DOOM']
